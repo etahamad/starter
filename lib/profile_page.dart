019f59da1aee829e1bac1bcc414f1c439fb43f28
etahamad <etahamad@icloud.com>
2022-06-19 16:44:37 +0200

    learning some flutter widgets to flirt with em
    widgets:
      - Image.assest
      - Divider
      - SizedBox
      - OutlinedButton
      - TextButton
      - ElevatedButton
      - Row
      - GestureDetector
      - Switch
      - Checkbox
        - Container:
      	- double.infinity
      	- TextStyle
      	- margin & padding

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -8,6 +8,8 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  bool isSwitched = false;
+  bool? isCheckbox = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +22,89 @@ class _ProfilePageState extends State<ProfilePage> {
             Navigator.of(context).pop();
           },
         ),
+      ),
+      body: Column(
+        children: [
+          Image.asset('images/orange.JPG'),
+          const SizedBox(height: 10),
+          const Divider(
+            color: Colors.black,
+          ),
+          Container(
+            margin: const EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(10.0),
+            color: Colors.blueGrey,
+            width: double.infinity,
+            child: const Center(
+              child: Text(
+                'This is a text widget',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: isSwitched ? Colors.green : Colors.grey,
+            ),
+            onPressed: () {
+              debugPrint('ElevatedButton pressed');
+            },
+            child: const Text('ElevatedButton'),
+          ),
+          OutlinedButton(
+            onPressed: () {
+              debugPrint('OutlinedButton pressed');
+            },
+            child: const Text('OutlinedButton'),
+          ),
+          TextButton(
+            onPressed: () {
+              debugPrint('TextButton pressed');
+            },
+            child: const Text('TextButton'),
+          ),
+          GestureDetector(
+            behavior: HitTestBehavior.opaque,
+            onTap: () {
+              debugPrint('GestureDetector tapped');
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: const [
+                Icon(
+                  Icons.local_fire_department,
+                  color: Colors.red,
+                ),
+                Text(
+                  'Row widget',
+                  style: TextStyle(color: Colors.red),
+                ),
+                Icon(
+                  Icons.local_fire_department,
+                  color: Colors.red,
+                ),
+              ],
+            ),
+          ),
+          Switch(
+              value: isSwitched,
+              onChanged: (bool value) {
+                setState(() {
+                  debugPrint('Switch value: $value');
+                  isSwitched = value;
+                });
+              }),
+          Checkbox(
+              value: isCheckbox,
+              onChanged: (bool? value) {
+                setState(() {
+                  debugPrint('Checkbox value: $value');
+                  isCheckbox = value;
+                });
+              }),
+        ],
       ),
     );
   }
